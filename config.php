52ca6405d3bf12f0555435955a010d5fbd8e0671
riddleforcop <topalyan2004@mail.ru>
2024-10-14 23:24:47 +0300

    dobavlenie_addBD

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+try {
+
+    $pdo = new PDO('sqlite:library.db');
+    
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Успешное подключение к базе данных!";
+} catch (PDOException $e) {
+
+    echo "Ошибка подключения к базе данных: " . $e->getMessage();
+}
+?>
